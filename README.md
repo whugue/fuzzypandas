40fe29f19f8ec3c70c6c816b09ffa595234ac640
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2017-01-16 23:15:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+FuzzyPandas brings the functionality of the [FuzzyWuzzy](https://github.com/seatgeek/fuzzywuzzy) approximate string matching library to [Pandas](http://pandas.pydata.org/) dataframe joins.
+
---
+++ fuzzypandas.py
@@ -0,0 +1,34 @@
+
+import time
+import pandas as pd
+from fuzzywuzzy import fuzz, process
+
+
+def brute_force_match(a, b, on, how, cutoff):
+	pass
+
+
+
+def fuzzy_merge(a, b, on, how="left", cutoff=0.6, match_function=brute_force_match):
+
+	##Merge dataframes regularly. Seperate out matches & nonmatches
+    a["in_a"] = 1 																	
+    b["in_b"] = 1 																	
+    
+    merged = pd.merge(a, b, on=key, how="outer") 									
+    matched = merged[(merged.in_a.notnull()) & (merged.in_b.notnull())].copy()		
+    nomatch_a = merged[(merged.in_a.notnull() | (merged.in_b.isnull()))].copy()		
+    nomatch_b = merged[(merged.in_a.isnull()) | (merged.in_b.notnull())].copy()		
+    
+    ##If no non-matches, we're done!. Return matched dataframe
+    if nomatch_a.shape[0]==0 | nomatch_b.shape[0]==0:
+    	return matched
+
+    ##Otherwise, proceed to fuzzy-matching non-exact matches
+	else:
+		xwalk = match_function(nomatch_a, nomatch_b, on=on, cutoff=cutoff)
+
+		fuzzy_merge1 = pd.merge(nonmatch_a, crosswalk, on=key+"_A", how="left")
+    	fuzzy_merge2 = pd.merge(fuzzy_merge1, nonmatch_b, on=key+"_B", how=how) 		
+
+    	return pd.concat([matched, fuzzy_merge_2], axis=0)
