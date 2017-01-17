af0d6a703bb8ce775286068a205a4d6351b9a9b4
Will Huguenin <willhuguenin@WillHugninsMBP2.fios-router.home>
2017-01-17 13:31:02 -0500

    reorg

--- fuzzypandas.py
+++ fuzzypandas.py
@@ -4,12 +4,36 @@ import pandas as pd
 from fuzzywuzzy import fuzz, process
 
 
-def brute_force_match(a, b, on, how, cutoff):
-	pass
+##Quick and dirty function to convert all text to string
+def decode_text(text):
+    if type(text) == unicode:
+        return text.encode("ascii", "replace")
+    else:
+        return text
+
+
+##Brute Force Matching (O(N^2) Time)
+def brute_force_match(a, b, on, how, cutoff, scorer=fuzz.ratio, show_score):
+
+    ##If multiple key variables, concatenate into single string
+    if len(on) > 1:
+        a["byvar"] = a[on].apply(lambda x: " ".join(x), axis=1)
+        b["byvar"] = b[on].apply(lambda x: " ".join(x), axis=1)
+    else:
+        a["byvar"] = a[on]
+        b["byvar"] = b[on]
+
+    matches = []
+    for row in a.byvar.unique():
+        match = process.extractOne(row, b.byvar.unique(), scorer=scorer)
+        matches.append({"byvar": row, "matched": match[0], "score": match[1]})
+
+    return pd.DataFrame(matches)
 
 
 
-def fuzzy_merge(a, b, on, how="left", cutoff=0.6, match_function=brute_force_match):
+
+def fuzzy_merge(a, b, on, how="left", cutoff=0.6, match_function=brute_force_match, show_score=True):
 
 	##Merge dataframes regularly. Seperate out matches & nonmatches
     a["in_a"] = 1 																	
@@ -26,9 +50,12 @@ def fuzzy_merge(a, b, on, how="left", cutoff=0.6, match_function=brute_force_mat
 
     ##Otherwise, proceed to fuzzy-matching non-exact matches
 	else:
-		xwalk = match_function(nomatch_a, nomatch_b, on=on, cutoff=cutoff)
+		xwalk = match_function(nomatch_a, nomatch_b, on=on, cutoff=cutoff, show_score=show_score)
 
 		fuzzy_merge1 = pd.merge(nonmatch_a, crosswalk, on=key+"_A", how="left")
     	fuzzy_merge2 = pd.merge(fuzzy_merge1, nonmatch_b, on=key+"_B", how=how) 		
 
     	return pd.concat([matched, fuzzy_merge_2], axis=0)
+
+
+
