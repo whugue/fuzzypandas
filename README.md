b1087da89929bb598fb73db42057df30ec819ae9
Will Huguenin <willhuguenin@Will-Huguenins-MacBook-Pro-2.local>
2017-01-17 20:37:42 -0500

    debug

--- README.md
+++ README.md
@@ -3,5 +3,15 @@
 FuzzyPandas brings the functionality of the [FuzzyWuzzy](https://github.com/seatgeek/fuzzywuzzy) approximate string matching library to [Pandas](http://pandas.pydata.org/) dataframe joins.
 
 
+#### TODO
+1. Add Prefix/ clean up output column names
+2. Concatenated exact match and fuzzy matched dataframes
+3. Run test script w/ pyprind on full U.S. News and IPEDS dataframes - measure time
+4. Write README.md
+5. Make repo public
+6. Assess match quality and write hierarchical matching algorithm
+7. Set up PyPI install
+
+
 #### Shameless Self Plug
 If you work for SeatGeek and you like what you see here, I'd love to talk to you about your open [data scientist](https://seatgeek.com/jobs/data_scientist) job. You can learn more about me [here](http://www.huguedata.com/) or contact me directly [here](mailto:will.huguenin@gmail.com)
--- fuzzypandas.py
+++ fuzzypandas.py
@@ -9,23 +9,21 @@ from fuzzywuzzy import fuzz, process
 Basic Match Function 
 """
 
-def basic_match(a, b, keyA, keyB, cutoff, quickmatch):
-
-    title = "Fuzzy Matching Results"
-    #bar = pyprind.ProgBar(50, monitor=True, title=title)
+def basic_match(keysA, keysB, cutoff, quickmatch):
+    #bar = pyprind.ProgBar(50, monitor=True)
 
     matches = []
-    for row in a[keyA].unique():
+    for keyA in keysA:
 
         if quickmatch==True:
-            match = process.extractOne(row, b[keyB].unique(), scorer=fuzz.ratio, score_cutoff=cutoff)
+            match = process.extractOne(keyA, keysB, scorer=fuzz.ratio, score_cutoff=cutoff)
         else:
-            match = process.extractOne(row, b[keyB].unique(), scorer=fuzz.WRatio, score_cutoff=cutoff)
+            match = process.extractOne(kayA, keysB, scorer=fuzz.WRatio, score_cutoff=cutoff)
 
         if match == None:
-            matches.append({"keyA": row, "keyB": None, "match_score": None})
+            matches.append({"keyA": keyA, "keyB": None, "match_score": None})
         else:
-            matches.append({"keyA": row, "keyB": match[0], "match_score": match[1]})
+            matches.append({"keyA": keyA, "keyB": match[0], "match_score": match[1]})
 
         #bar.update()
 
@@ -36,7 +34,7 @@ def basic_match(a, b, keyA, keyB, cutoff, quickmatch):
 Hierarchical Match Function
 """
 
-def hier_match(a, b, cutoff, quickmatch):
+def hier_match(keysA, keysB, cutoff, quickmatch):
     pass
 
 
@@ -46,47 +44,37 @@ Dataframe Merge Function
 
 def fuzzy_merge(a, b, on, how="left", matcher=basic_match, cutoff=60, quickmatch=True):
 
-	##Perform exact match on dataframes A and B
-    a["in_a"] = 1 																	
+    ##Perform exact match on dataframes A and B
+    a["in_a"] = 1                                                                   
     b["in_b"] = 1
-
     merged = pd.merge(a, b, on=on, how="outer")
 
 
     ##Split results into matches & nonmatches 
     matched = merged[(merged.in_a.notnull()) & (merged.in_b.notnull())].copy()
-    nomatch_a = merged[(merged.in_a.notnull() & (merged.in_b.isnull()))].copy()		
+    nomatch_a = merged[(merged.in_a.notnull() & (merged.in_b.isnull()))].copy()     
     nomatch_b = merged[(merged.in_a.isnull()) & (merged.in_b.notnull())].copy()
 
-    matched["match_score"] = 100 ##All Exact matches get a match score of 100% by default
+    matched["match_score"] = 100 ##exact matches get a defacto match score of 100%
 
 
     ##If no non-matches, we're done!. Return matched dataframe
     if min(nomatch_a.shape[0], nomatch_b.shape[0])==0:
         return matched
 
-    ##Otherwise, try to fuzzy match the nonmatches and append any new matches
+    ##Otherwise, use fuzzy matching
     else:
         if type(on) == list:
-            nomatch_a["keyA"] = a[on].apply(lambda x: " ".join(x), axis=1)
-            nomatch_b["keyB"] = b[on].apply(lambda x: " ".join(x), axis=1)
+            nomatch_a["keyA"] = nomatch_a[on].apply(lambda x: " ".join(x), axis=1)
+            nomatch_b["keyB"] = nomatch_b[on].apply(lambda x: " ".join(x), axis=1)
         else:
-            nomatch_a["keyA"] = a[on]
-            nomatch_b["keyB"] = b[on]
+            nomatch_a["keyA"] = nomatch_a[on]
+            nomatch_b["keyB"] = nomatch_b[on]
 
-        xwalk = matcher(nomatch_a, nomatch_b, keyA="keyA", keyB="keyB", cutoff=cutoff, quickmatch=quickmatch)
+        xwalk = matcher(nomatch_a.keyA.unique(), nomatch_b.keyB.unique(), cutoff=cutoff, quickmatch=quickmatch)
 
         fuzzy_merge1 = pd.merge(nomatch_a, xwalk, on="keyA", how="left")
-        fuzzy_merge2 = pd.merge(fuzzymerge1, nomatch_b, on="keyB", how=how)
+        fuzzy_merge2 = pd.merge(fuzzy_merge1, nomatch_b, on="keyB", how=how)
 
         return fuzzy_merge2
-        #return pd.concat([matched, fuzzy_merge2], axis=0)
-
-
-
-
-
-
-
-
-
+        #return pd.concat([matched, fuzzy_merge2], axis=0) ##debug this, then run on script w/ whole dataset
